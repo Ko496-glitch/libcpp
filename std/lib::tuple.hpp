cfedc1fa61d8c46bb34ae744e650b35b9417e3a8
kartikohlan <kartik7ohlan@gmail.com>
2026-03-15 19:31:39 -0400

    added namespace

--- std/lib::tuple.hpp
+++ std/lib::tuple.hpp
@@ -1,24 +1,23 @@
 
-#include<new>
-#include<utility>
+#include <new>
+#include <utility>
 
+namespace lib {
+template <typename... types> class Tuple;
 
-template<typename... types>
-class Tuple;
+template <typename Head, typename... Tail> class Tuple<head, tail> {
 
-template<typename Head,typename... Tail>
-class Tuple<head,tail>{
-  
-  private:
-    Head head;
-    Tuple<Tail...> tail;
+private:
+  Head head;
+  Tuple<Tail...> tail;
 
-  public:
+public:
+  Tuple(const Head &head, const Tuple<Tail...> &tail)
+      : head(head), tail(tail) {}
 
-    Tuple(const Head& head, const Tuple<Tail...> &tail) :head(head),tail(tail){}
-
-    Head &getHead(){return head;}
-    Head const &getHead(){return  head;}
-    Tuple<Tail...>&getTail(){return tail;}
-    Tuple<Tail...> const &getTail(){return tail;}
+  Head &getHead() { return head; }
+  Head const &getHead() { return head; }
+  Tuple<Tail...> &getTail() { return tail; }
+  Tuple<Tail...> const &getTail() { return tail; }
 };
+} // namespace lib
