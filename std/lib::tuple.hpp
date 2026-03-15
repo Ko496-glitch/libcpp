358f27eed836fa7a1317ff9e6b61d3d26e5751b6
kartikohlan <kartik7ohlan@gmail.com>
2026-03-15 19:23:56 -0400

    adding lib::tuple support

---
+++ std/lib::tuple.hpp
@@ -0,0 +1,24 @@
+
+#include<new>
+#include<utility>
+
+
+template<typename... types>
+class Tuple;
+
+template<typename Head,typename... Tail>
+class Tuple<head,tail>{
+  
+  private:
+    Head head;
+    Tuple<Tail...> tail;
+
+  public:
+
+    Tuple(const Head& head, const Tuple<Tail...> &tail) :head(head),tail(tail){}
+
+    Head &getHead(){return head;}
+    Head const &getHead(){return  head;}
+    Tuple<Tail...>&getTail(){return tail;}
+    Tuple<Tail...> const &getTail(){return tail;}
+};
